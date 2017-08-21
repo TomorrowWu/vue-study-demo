9e3ecc4dfa3228c5efb78bee2ccac69ac2e0f063
吴名 <wuming497735138@gmail.com>
2017-08-21 23:23:12 +0800

    左侧导航出来了

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-    <i class="icon iconfont icon-dianpu"></i>
-    <i class="icon iconfont icon-gongnengjianyi"></i>
-    <i class="icon iconfont icon-shouji"></i>
-    <router-view></router-view>
+    <!--<i class="icon iconfont icon-dianpu"></i>-->
+    <!--<i class="icon iconfont icon-gongnengjianyi"></i>-->
+    <!--<i class="icon iconfont icon-shouji"></i>-->
+
+    <!--左侧导航-->
+    <left-nav></left-nav>
+
+    <div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import leftNav from '@/components/common/leftNav'
+
+  export default {
+    name: 'app',
+    components: {
+      leftNav
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  color: #980988;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    color: #980988;
+    margin-top: 60px;
+  }
 </style>
