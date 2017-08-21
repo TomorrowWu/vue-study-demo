732b69585a3b24f65fe56cb314bf0abf0dfbf3b5
吴名 <wuming497735138@gmail.com>
2017-08-22 00:22:59 +0800

    1,引入ElementUI 2,使用el-row和el-col实现大致布局

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="pos">
-    Hello Pos Demo!
+    <!--Hello Pos Demo!-->
+    <el-row>
+      <el-col :span='7' class="pos-order" id="order-list">
+        我是订单栏
+      </el-col>
+      <el-col>
+        我是产品栏
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Pos'
+    name: 'Pos',
+    mounted: function () {
+      var orderHeight = document.body.clientHeight
+      console.log(orderHeight)
+      document.getElementById('order-list').style.height = orderHeight = orderHeight + 'px'
+    }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .pos-order {
+    background-color: #F9FAFC;
+    border-right: 1px solid #C0CCDA;
+    height: 100%;
+  }
 </style>
