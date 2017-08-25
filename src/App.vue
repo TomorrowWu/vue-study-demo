e1af6cc25391ad9c58e14140151ce37da10bd8bb
吴名 <wuming497735138@gmail.com>
2017-08-25 14:59:22 +0800

    字体改为微软雅黑

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Microsoft YaHei','Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
